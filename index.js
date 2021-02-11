650a1a14f4684870f1ad6594ef6427962466c303
TatiKolpakova <deertinti@gmail.com>
2021-02-12 01:40:17 +0500

    added focus onclick document

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 EMPTY_LINE = `<div class="">
 <span class="username-text">Client@alisherrusinov-site</span> <span class="directory-text">:~</span><span class="regular-text">$</span>
-<input type="text" class="text-input" >
 </div>`
+
+
+
+document.addEventListener("click", e => {
+    document.getElementById('maininput').focus()
+  })
+
 function inputpress(e) {
     if (e.keyCode == 13) {
         text = document.getElementById('maininput').value
