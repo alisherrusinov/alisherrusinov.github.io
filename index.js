9d93073924785ebbc900c0d82ec74027486a4dca
TatiKolpakova <deertinti@gmail.com>
2021-02-12 01:48:44 +0500

    added ls command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ EMPTY_LINE = `<div class="">
 <span class="username-text">Client@alisherrusinov-site</span> <span class="directory-text">:~</span><span class="regular-text">$</span>
 </div>`
 
-
+LS_STRING = `<span class="directory-text">project 1</span> <br><span class="directory-text">project 2</span>`
 
 document.addEventListener("click", e => {
     document.getElementById('maininput').focus()
@@ -11,8 +11,10 @@ document.addEventListener("click", e => {
 function inputpress(e) {
     if (e.keyCode == 13) {
         text = document.getElementById('maininput').value
-        console.log(text)
         document.getElementById('maininput').value = ""
         document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", EMPTY_LINE);
+        if(text == 'ls'){
+            document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", LS_STRING);
+        }
     }
 }
