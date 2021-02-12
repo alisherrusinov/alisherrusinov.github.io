a5c0b29b12134907e83d596e7838c277b9d0110d
alisherrusinov <alisher.nugmanoff@gmail.com>
2021-02-12 15:17:54 +0500

    added saving prev command

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-EMPTY_LINE = `<div class="">
-<span class="username-text">Client@alisherrusinov-site</span> <span class="directory-text">:~</span><span class="regular-text">$</span>
-</div>`
 
-LS_STRING = `<span class="directory-text">project 1</span> <br><span class="directory-text">project 2</span>`
+
+LS_STRING_HOME = `<span class="directory-text">About me</span> <br><span class="directory-text">Projects</span>`
+
+LS_STRING_PROJECTS = `<span class="directory-text">project 1</span> <br><span class="directory-text">project 2</span>`
+
+CURRENT_DIRECTORY = 'home'
 
 document.addEventListener("click", e => {
     document.getElementById('maininput').focus()
-  })
+})
 
 function inputpress(e) {
     if (e.keyCode == 13) {
         text = document.getElementById('maininput').value
         document.getElementById('maininput').value = ""
+
+        EMPTY_LINE = `<div class="">
+        <span class="username-text">Client@alisherrusinov-site</span> <span class="directory-text">:~</span><span class="regular-text">$</span>
+        <input type="text" class="text-input" id='previous' value="${text}">
+        </div>`
         document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", EMPTY_LINE);
-        if(text == 'ls'){
-            document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", LS_STRING);
+
+        if (text == 'ls') {
+            if (CURRENT_DIRECTORY == 'home') {
+                document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", LS_STRING_HOME);
+            }
+            else if (CURRENT_DIRECTORY = 'projects') {
+                document.getElementById('mainblock').insertAdjacentHTML("beforeBegin", LS_STRING_PROJECTS);
+            }
+        }
+        else if (text == 'cd Projects') {
+            CURRENT_DIRECTORY = 'projects'
+        }
+        else if (text == 'cd Home') {
+            CURRENT_DIRECTORY = 'home'
         }
     }
 }
